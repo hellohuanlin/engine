36a1a6909fa9dadd7cf37a1f3c955b04650a6134
Huan Lin <hellohuanlin@gmail.com>
2024-06-03 14:48:08 -0700

    with local engine marker

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -809,6 +809,11 @@ static void SendFakeTouchEvent(UIScreen* screen,
   }
 
   [super viewDidLoad];
+
+
+  UIView* dummyView = [[[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)] autorelease];
+  dummyView.backgroundColor = UIColor.greenColor;
+  [self.view addSubview:dummyView];
 }
 
 - (void)addInternalPlugins {
